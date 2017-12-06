500fdca25b3faf958ff0bffa69ace5a6e9a5be75
yewl <yewl@dxy.cn>
2017-12-06 15:46:24 +0800

    Kosaraju-Sharir 算法实现

---
+++ src/main/java/com/radium4ye/algorithm/graph/direction/KosarajuSCC.java
@@ -0,0 +1,89 @@
+package com.radium4ye.algorithm.graph.direction;
+
+import com.radium4ye.structure.MyStack;
+import lombok.Getter;
+
+/**
+ * Kosaraju-Sharir 算法 （实现容易，理解困难）
+ * 强连通计算相关
+ * <p>
+ * 该算法使用的是邻接表来组成图
+ * 每次访问都和 V+E 成正比，时间复杂度 O(V+E)
+ * <p>
+ * GR 图 = G 图的逆向图
+ * s->v = 图中包含 s 到 v 的路径
+ * 证明：
+ * 根据 dfs 搜索规则知：每个和 s 的连通顶点 v，都会再构造函数中被 dfs(G,s) 所访问到
+ * 只要证明 G 中包含 v->s 即可，等价于 GR 中存在 s->v
+ * 按照逆后续深度优选搜索得知，在 GR 中进行 dfs 中 dfs(GR,v) 必先比 dfs(GR,s) 先结束，
+ * 而且在 G 图中 s->v ,所以在 GR 图中 v->s ,所以 dfs(GR,v) 不可能比 dfs(GR,s) 先发生(而且先结束)，只有可能包含在 dfs(GR,s) 中，
+ * 这种情况意味着 GR 图中有一条路径 s->v,G 图中 v->s
+ *
+ * @author radium4ye
+ */
+@Getter
+public class KosarajuSCC {
+    /**
+     * 节点是否检查过了
+     */
+    private boolean[] marked;
+
+    /**
+     * 连通分量数
+     */
+    private int count;
+
+    /**
+     * 每个顶点的 连通分量标识符
+     */
+    private int[] stronglyComponent;
+
+    /**
+     * 算法构造，传入有向图
+     * @param diGraph   有向图
+     */
+    public KosarajuSCC(DiGraph diGraph) {
+        marked = new boolean[diGraph.getVertices()];
+        stronglyComponent = new int[diGraph.getVertices()];
+
+        //获取反向图的逆后续排列
+        Topological topological = new Topological(diGraph.reverse());
+        MyStack<Integer> reversePost = topological.getReversePostOrder();
+
+        //根据反向图的逆后续来遍历原图
+        while (!reversePost.isEmpty()) {
+            Integer vertices = reversePost.pop();
+            if (!marked[vertices]) {
+                dfs(diGraph, vertices);
+                count++;
+            }
+        }
+
+    }
+
+    /**
+     * 进行深度优先搜索
+     * @param diGraph   有向图
+     * @param vertices  顶点
+     */
+    public void dfs(DiGraph diGraph, int vertices) {
+        marked[vertices] = true;
+        stronglyComponent[vertices] = count;
+
+        diGraph.adj(vertices).forEach(nextVertices -> {
+            if (!marked[nextVertices]) {
+                dfs(diGraph, nextVertices);
+            }
+        });
+    }
+
+    /**
+     * 判断两个顶点是否强连通
+     * @param v1    顶点1
+     * @param v2    顶点2
+     * @return      是否强连通
+     */
+    public boolean stronglyConnect(int v1, int v2) {
+        return stronglyComponent[v1] == stronglyComponent[v2];
+    }
+}
--- src/test/java/com/radium4ye/algorithm/graph/direction/DiGraphTest.java
+++ src/test/java/com/radium4ye/algorithm/graph/direction/DiGraphTest.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Iterator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -16,48 +18,72 @@ public class DiGraphTest {
     private DiGraph diGraph;
 
     @Before
-    public void init(){
+    public void init() {
         diGraph = new DiGraph(10);
-        diGraph.addEdge(0,1);
-        diGraph.addEdge(2,0);
-        diGraph.addEdge(3,2);
-        diGraph.addEdge(4,3);
-        diGraph.addEdge(4,2);
-        diGraph.addEdge(0,5);
-        diGraph.addEdge(3,5);
-        diGraph.addEdge(6,0);
-        diGraph.addEdge(6,4);
+        diGraph.addEdge(0, 1);
+        diGraph.addEdge(2, 0);
+//        diGraph.addEdge(2,3);
+        diGraph.addEdge(3, 2);
+        diGraph.addEdge(4, 3);
+        diGraph.addEdge(4, 2);
+        diGraph.addEdge(0, 5);
+//        diGraph.addEdge(5,4);
+        diGraph.addEdge(3, 5);
+        diGraph.addEdge(6, 0);
+        diGraph.addEdge(6, 4);
 
     }
 
     @Test
     public void reverse() throws Exception {
         Iterator<Integer> iterator = diGraph.adj(0).iterator();
-        assertEquals(iterator.next(),5);
-        assertEquals(iterator.next(),1);
+        assertEquals(iterator.next(), 5);
+        assertEquals(iterator.next(), 1);
 
         DiGraph diGraphReverse = diGraph.reverse();
         iterator = diGraphReverse.adj(1).iterator();
-        assertEquals(iterator.next(),0);
+        assertEquals(iterator.next(), 0);
         iterator = diGraphReverse.adj(5).iterator();
         iterator.next();
-        assertEquals(iterator.next(),0);
+        assertEquals(iterator.next(), 0);
 
     }
 
     @Test
     public void directedDFS() throws Exception {
-        DirectedDFS directedDFS = new DirectedDFS(diGraph,0);
-        assertEquals(directedDFS.marked(2),true);
-        assertEquals(directedDFS.marked(6),false);
+        DirectedDFS directedDFS = new DirectedDFS(diGraph, 0);
+        assertEquals(directedDFS.marked(2), true);
+        assertEquals(directedDFS.marked(6), false);
     }
 
     @Test
     public void topological() throws Exception {
         Topological topological = new Topological(diGraph);
-        MyStack<Integer> myStack = topological.getOppositionPostOrder();
-        while (!myStack.isEmpty()){
+        MyStack<Integer> myStack = topological.getReversePostOrder();
+        while (!myStack.isEmpty()) {
             System.out.println(myStack.pop());
         }
+    }
+
+    @Test
+    public void kosarajuSCC() throws Exception {
+        diGraph.addEdge(2, 3);
+        diGraph.addEdge(5,4);
+
+        diGraph.addEdge(8,9);
+        diGraph.addEdge(9,8);
+        KosarajuSCC kosarajuSCC = new KosarajuSCC(diGraph);
+
+        assertEquals(kosarajuSCC.stronglyConnect(4,5),true);
+
+        IntStream.range(0, kosarajuSCC.getCount()).forEach(i -> {
+            System.out.print("\t\n" + i + ":");
+            for (int j = 0; j < diGraph.getVertices(); j++) {
+                int value = kosarajuSCC.getStronglyComponent()[j];
+                if( value == i){
+                    System.out.print(" " + j);
+                }
+            }
+        });
     }
 }
