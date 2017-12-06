c5acbeef01e6ebc500dc64e6c374e64a704fa229
yewl <yewl@dxy.cn>
2017-12-06 10:52:00 +0800

    拓扑排序

---
+++ src/main/java/com/radium4ye/algorithm/graph/direction/Topological.java
@@ -0,0 +1,77 @@
+package com.radium4ye.algorithm.graph.direction;
+
+import com.radium4ye.structure.MyQueue;
+import com.radium4ye.structure.MyStack;
+import lombok.Getter;
+
+import java.util.Queue;
+
+/**
+ * 拓扑排序
+ * 前序：再递归调用之前加入队列
+ * 后序：再递归调用之后加入队列
+ * 逆后序：再递归调用之后压入栈  用于计算时间的先后顺序
+ *
+ * @author radium4ye
+ */
+@Getter
+public class Topological {
+
+    /**
+     * 前序
+     */
+    private Queue<Integer> preOrder;
+
+    /**
+     * 后序
+     */
+    private Queue<Integer> postOrder;
+
+    /**
+     * 逆后序
+     */
+    private MyStack<Integer> oppositionPostOrder;
+
+    /**
+     * 是否检查过
+     */
+    private boolean[] marked;
+
+    /**
+     * 构建拓扑排序
+     * @param diGraph
+     */
+    public Topological(DiGraph diGraph) {
+        preOrder = new MyQueue<>();
+        postOrder = new MyQueue<>();
+        oppositionPostOrder = new MyStack<>();
+        marked = new boolean[diGraph.getVertices()];
+
+        for (int i = 0; i < diGraph.getVertices(); i++) {
+            if (!marked[i]) {
+                dfs(diGraph, i);
+            }
+        }
+    }
+
+    /**
+     * 进行深度优先搜索
+     *
+     * @param diGraph  有向图
+     * @param vertices 起始顶点
+     */
+    private void dfs(DiGraph diGraph, final int vertices) {
+        marked[vertices] = true;
+        preOrder.add(vertices);
+        //遍历该节点的所有邻节点
+        diGraph.adj(vertices)
+                .forEach(nextVertices -> {
+                    if (!marked[nextVertices]) {
+                        dfs(diGraph, nextVertices);
+                    }
+                });
+        postOrder.add(vertices);
+        oppositionPostOrder.push(vertices);
+    }
+
+}
--- src/test/java/com/radium4ye/algorithm/graph/direction/DiGraphTest.java
+++ src/test/java/com/radium4ye/algorithm/graph/direction/DiGraphTest.java
@@ -1,5 +1,6 @@
 package com.radium4ye.algorithm.graph.direction;
 
+import com.radium4ye.structure.MyStack;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,12 +20,10 @@ public class DiGraphTest {
         diGraph = new DiGraph(10);
         diGraph.addEdge(0,1);
         diGraph.addEdge(2,0);
-        diGraph.addEdge(2,3);
         diGraph.addEdge(3,2);
         diGraph.addEdge(4,3);
         diGraph.addEdge(4,2);
         diGraph.addEdge(0,5);
-        diGraph.addEdge(5,4);
         diGraph.addEdge(3,5);
         diGraph.addEdge(6,0);
         diGraph.addEdge(6,4);
@@ -51,5 +50,14 @@ public class DiGraphTest {
         DirectedDFS directedDFS = new DirectedDFS(diGraph,0);
         assertEquals(directedDFS.marked(2),true);
         assertEquals(directedDFS.marked(6),false);
+    }
+
+    @Test
+    public void topological() throws Exception {
+        Topological topological = new Topological(diGraph);
+        MyStack<Integer> myStack = topological.getOppositionPostOrder();
+        while (!myStack.isEmpty()){
+            System.out.println(myStack.pop());
+        }
     }
 }
