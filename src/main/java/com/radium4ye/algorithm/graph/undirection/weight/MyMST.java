4ff4190c5676d2e36814f063c2ed6d0201e6d9c9
yewl <yewl@dxy.cn>
2017-12-07 16:35:39 +0800

    改成使用堆排序的优先队列后，性能显著提升

--- src/main/java/com/radium4ye/algorithm/graph/undirection/weight/MyMST.java
+++ src/main/java/com/radium4ye/algorithm/graph/undirection/weight/MyMST.java
@@ -1,21 +1,22 @@
 package com.radium4ye.algorithm.graph.undirection.weight;
 
-import com.radium4ye.algorithm.sort.SelectSort;
+import com.radium4ye.algorithm.sort.pq.HeapPriorityQueue;
 import com.radium4ye.structure.MyStack;
 import lombok.Getter;
 
 /**
  * 最小生成树
- * 自己实现
+ * 自己实现，感觉在最后的遍的时候，会遍历很多无用的数据
  * 将所有边进行降序排序，并遍历一边。选择为连通的边
- *
- * 空间和 vertices + edge 成正比
+ * <p>
+ * 空间和 edge 成正比
+ * 时间和 edge * log edge 成正比
  * 时间主要和排序算法有关
  *
  * @author radium4ye
  */
 @Getter
-public class MST {
+public class MyMST {
 
     /**
      * 已经检查过的顶点
@@ -33,21 +34,24 @@ public class MST {
      *
      * @param graph 无向加权图
      */
-    public MST(EdgeWeightGraph graph) {
+    public MyMST(EdgeWeightGraph graph) {
         marked = new boolean[graph.getVertices()];
         selectEdges = new MyStack<>();
 
         //获取全部边，并进行降序排序
         MyStack<Edge> myStack = graph.edges();
-        Edge[] array = new Edge[myStack.size()];
-        array = myStack.toArray(array);
+        //获取最小边
+        HeapPriorityQueue<Edge> queue = new HeapPriorityQueue<>(myStack.size(), (o1, o2) -> -1 * o1.compareTo(o2));
+        //没实现这个方法 (ノಠ益ಠ)ノ彡┻━┻
+//        queue.addAll(myStack);
+        while (!myStack.isEmpty()){
+            queue.offer(myStack.pop());
+        }
 
-        //降序排序，获得最小边 性能低下
-        SelectSort.sort(array, false);
         //选择边数
         int selectNum = 0;
-        for (Edge edge : array) {
-
+        while (!queue.isEmpty()) {
+            Edge edge = queue.poll();
             //判断该边的两个顶点是否连通
             if (!marked[edge.getVertices1()] || !marked[edge.getVertices2()]) {
 
--- src/test/java/com/radium4ye/algorithm/graph/undirection/weight/EdgeWeightGraphTest.java
+++ src/test/java/com/radium4ye/algorithm/graph/undirection/weight/EdgeWeightGraphTest.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import static org.junit.Assert.*;
-
 /**
  * @author radium4ye
  */
@@ -17,18 +15,7 @@ public class EdgeWeightGraphTest {
 
     @Before
     public void setUp() throws Exception {
-//        graph = new EdgeWeightGraph(6);
-//        graph.addEdge(new Edge(0,1,1.0));
-//        graph.addEdge(new Edge(0,2,1.2));
-//        graph.addEdge(new Edge(1,3,0.7));
-//        graph.addEdge(new Edge(2,3,0.6));
-//        graph.addEdge(new Edge(1,5,1.3));
-//
-//        graph.addEdge(new Edge(1,4,2.0));
-//        graph.addEdge(new Edge(4,5,0.4));
-//        graph.addEdge(new Edge(3,5,0.5));
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(EdgeWeightGraphTest.class.getClassLoader().getResourceAsStream("1000EWG.txt")));
+        BufferedReader br = new BufferedReader(new InputStreamReader(EdgeWeightGraphTest.class.getClassLoader().getResourceAsStream("10000EWG.txt")));
         Integer vertices = Integer.valueOf(br.readLine());
         graph = new EdgeWeightGraph(vertices);
         Integer edge = Integer.valueOf(br.readLine());
@@ -40,14 +27,9 @@ public class EdgeWeightGraphTest {
         }
     }
 
-//    @Test
-//    public void constructor() throws Exception {
-//
-//    }
-
     @Test
     public void mst() throws Exception {
-        MST mst = new MST(graph);
+        MyMST mst = new MyMST(graph);
         mst.getSelectEdges().forEach(edge -> {
             System.out.println(edge);
         });
