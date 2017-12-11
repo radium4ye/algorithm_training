6c32a6acd51bcc0c09e71132e7d94e654aeeea27
yewl <yewl@dxy.cn>
2017-12-11 18:43:00 +0800

    并发程序调度 待实现

---
+++ src/main/java/com/radium4ye/algorithm/graph/direction/weight/ParallelTaskSchedule.java
@@ -0,0 +1,12 @@
+package com.radium4ye.algorithm.graph.direction.weight;
+
+/**
+ * 别名：TopologicalLP
+ * 基于拓扑排序的最长路径计算方法
+ * 这里用于解决并行调度任务最短时间计算问题
+ *
+ * @author radium4ye
+ */
+public class ParallelTaskSchedule {
+
+}
