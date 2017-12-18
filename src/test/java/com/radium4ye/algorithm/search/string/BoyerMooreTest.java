d0c378c3e54a650e9f1004794f63ce0efa46af48
yewl <yewl@dxy.cn>
2017-12-18 15:37:58 +0800

    添加比较

--- src/test/java/com/radium4ye/algorithm/search/string/BoyerMooreTest.java
+++ src/test/java/com/radium4ye/algorithm/search/string/BoyerMooreTest.java
@@ -1,6 +1,9 @@
 package com.radium4ye.algorithm.search.string;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -17,4 +20,41 @@ public class BoyerMooreTest {
         assertEquals(BoyerMoore.match(pattern,target),-1);
     }
 
+    private String target;
+    private String pattern;
+    @Before
+    public void setup() throws Exception {
+        pattern = getRandomStr(100);
+        target = getRandomStr(100000000);
+    }
+
+    @Test
+    public void compare() throws Exception {
+        long l1 = System.currentTimeMillis();
+        System.out.println(BoyerMoore.match(target,pattern));
+        long l2 = System.currentTimeMillis();
+        System.out.println("BoyerMoore:time:" + (l2 - l1));
+
+        long l3 = System.currentTimeMillis();
+        System.out.println(target.indexOf(pattern));
+        long l4 = System.currentTimeMillis();
+        System.out.println("origin:time:" + (l4 - l3));
+    }
+
+
+    final static String CHARS = "abcdegjhijklmnopqrsquvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    /**
+     * 获得一定长度的随机字符串
+     * @param len
+     * @return
+     */
+    public static String getRandomStr(int len){
+        StringBuffer sb = new StringBuffer();
+        Random random = new Random();
+
+        for (int i = 0; i < len; i++) {
+            sb.append(CHARS.charAt(random.nextInt(CHARS.length())));
+        }
+        return sb.toString();
+    }
 }
